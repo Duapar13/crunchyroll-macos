d7303e2374df928b97b8d6cd73bb34824362b472
Duapar <duapar@Air-de-Duapar.lan>
2024-11-02 17:31:05 +0100

    fix: save session token

--- app/src/index.js
+++ app/src/index.js
@@ -8,11 +8,18 @@ function createWindow() {
     height: 600,
     icon: path.join(__dirname, '../assets/icons/logo.icns'), // Chemin vers l'icône .icns
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      webSecurity: true,
     },
   });
 
-  mainWindow.loadURL('https://sso.crunchyroll.com/fr/login');
+  // Définir un User-Agent compatible
+  mainWindow.webContents.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36');
+
+  // Chargez l'URL souhaitée
+  mainWindow.loadURL('https://www.crunchyroll.com/fr/login');
 }
 
 app.whenReady().then(createWindow);
